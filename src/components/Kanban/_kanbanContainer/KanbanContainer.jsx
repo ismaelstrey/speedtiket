090452da7ddd0a8ff7d6dc336e2f63016cb82e89
Ismael Strey Pereira <ismaelstrey@hotmail.com>
2023-02-20 20:06:58 -0300

    update resposividade

--- src/components/Kanban/_kanbanContainer/KanbanContainer.jsx
+++ src/components/Kanban/_kanbanContainer/KanbanContainer.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-const KanbanContainer =({children})=><div className='flex w-full h-full bg-yellow-100'>{children}</div>
+const KanbanContainer =({children})=><div className='flex md:flex-col sm:flex-col lg:flex-row w-full h-full bg-yellow-100'>{children}</div>
 
 export  {KanbanContainer};
