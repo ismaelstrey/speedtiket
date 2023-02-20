7942694b3017e4a71a7871f9df4eb8c2a09ac915
Ismael Strey Pereira <ismaelstrey@hotmail.com>
2023-02-20 13:49:37 -0300

    update form color

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -12,14 +12,14 @@ const Menu = ()  =>  {
   const [menu, setMenu] =useState(true)
   const [abrirForm, setAbrirForm] =useState(false)
 
-  const AbrirFormulario = () => <div className="fixed flex w-full h-screen items-center justify-center bg-opacity-30 backdrop-blur-sm inset-0"><NewTiket/></div>
+  const AbrirFormulario = () => <div className="fixed flex  w-full h-screen items-center justify-center bg-opacity-30 backdrop-blur-sm inset-0"><NewTiket/></div>
 
 return(
   
  
 <div>
   {abrirForm && <AbrirFormulario />}
-   <div className="fixed bottom-10 right-10 rounded-full bg-green-400 p-2 hover:bg-green-900 cursor-pointer z-20" onClick={()=>setMenu(!menu)}><CgMenuRound/>
+   <div className={`fixed rounded-full bg-green-400 p-2 hover:bg-white cursor-pointer z-20 ${menu ? 'bottom-10 right-10 ':'bottom-0 right-0 '}`} onClick={()=>setMenu(!menu)}><CgMenuRound/>
 </div>
      {menu &&  <div className=" bg-black fixed p-10 rounded-full  bottom-4 right-4 z-10 border-2 border-white ">
    <span className="bg-green-600 p-2 rounded-full fixed bottom-10 right-20 hover:border-2 hover: border-white cursor-pointer" onClick={()=>setAbrirForm(!abrirForm)}><IoIosAddCircle/></span>
--- src/components/Tikets/NewTiket/NewTiket.jsx
+++ src/components/Tikets/NewTiket/NewTiket.jsx
@@ -12,47 +12,48 @@ const NewTiket = () => {
 
     //o register recebe o nome do input, e os errors apresentam na tela os erros de validação
 
-    <form onSubmit={handleSubmit(loginUser)} className="m-20  flex w-full justify-center content-center items-center bg-slate-100 rounded  border-2 border-black">
+    <form onSubmit={handleSubmit(loginUser)} className="m-20 container text-lg text-white  flex w-full justify-center content-center items-center bg-slate-800 rounded-2xl  border-2 border-black">
 
-    <div className="flex flex-col w-full ">
-    <div className="flex w-full">
+    <div className="flex flex-col w-full   ">
+     <span className="flex w-full items-center justify-center content-center mb-10 bg-black rounded-t-2xl h-10 border-b-2 border-white"> <h2>Cadastro de empresa</h2></span>
+    <div className="flex sm:flex-col lg:flex-row w-full">
 
-<div className="flex w-full  p-4 content-center items-start justify-start flex-1">
+<div className="flex  w-full  p-4 content-center items-center justify-start">
   <label className="px-3 ">Tipo:</label>
-  <select {...register("tipo_empresa", { required: true })}  className="bg-slate-100 flex  border-b-2 border-solid border-black select-none outline-none focus:outline-0 ">
+  <select {...register("tipo_empresa", { required: true })}  className="text-black form-select flex-1  border-solid  select-none outline-none focus:outline-0 rounded-full border-0 ">
     <option value="fisico">Fisico</option>
     <option value="juridico" selected>Juridico</option>
     <option value="" selected></option>
   </select>
 </div>
 
-<div className="flex w-full  p-4 content-center items-start justify-start">
+<div className="flex w-full  p-4 content-center items-center justify-center">
   <label className="px-3 ">Rasão social:</label>
-  <input  {...register("rasao_social", { required: true })} className="bg-slate-100 flex-1   border-b-2 border-solid border-black select-none outline-none focus:outline-0" />
+  <input  {...register("rasao_social", { required: true })} className="form-input text-black rounded-full  flex-1   border-0 border-solid  select-none outline-none focus:outline-0" />
   {errors.name && <span>Nome</span>}
 </div>
 
-<div className="flex w-full p-4">
+<div className="flex w-full p-4 justify-center items-center">
   <label className="px-3">Nome fantasia:</label>
-  <input  {...register("nome_fantasia", { required: true })} className="bg-slate-100 flex-1 border-b-2 border-solid border-black select-none outline-none focus:outline-0 " />
+  <input  {...register("nome_fantasia", { required: true })} className="form-input flex-1 text-black rounded-full border-0 select-none outline-none focus:outline-0 " />
   {errors.name && <span>Nome</span>}
 </div>
 </div>
 
-<div className="flex w-full">
-<div className="flex p-4 w-full ">
+<div className="flex w-full sm:flex-col lg:flex-row">
+<div className="flex p-4 w-full items-center justify-center ">
   <label className="px-3">CNPJ:</label>
-  <input  {...register("cnpj", { required: true })} className="bg-slate-100 flex flex-1  border-b-2 border-solid border-black select-none outline-none focus:outline-0 " />
+  <input  {...register("cnpj", { required: true })} className="form-input text-black  rounded-full border-0 flex-1 select-none outline-none focus:outline-0 " />
   {errors.name && <span>CNPJ</span>}
 </div>
-<div className="flex p-4 w-full">
+<div className="flex p-4 w-full  items-center justify-center ">
   <label className="px-3">IE:</label>
-  <input  {...register("ie", { required: true })} className="bg-slate-100 flex  flex-1 border-b-2 border-solid border-black select-none outline-none focus:outline-0 " />
+  <input  {...register("ie", { required: true })} className="form-input text-black rounded-full  flex-1 border-0  select-none outline-none focus:outline-0 " />
   {errors.name && <span>IE</span>}
 </div>
-<div className="flex w-full  p-4 content-center items-start justify-start flex-1">
+<div className="flex w-full  p-4 content-center items-center justify-start">
   <label className="px-3 ">Matriz:</label>
-  <select {...register("matriz", { required: true })}  className="bg-slate-100 flex flex-1  border-b-2 border-solid border-black select-none outline-none focus:outline-0 ">
+  <select {...register("matriz", { required: true })}  className="form-select text-black rounded-full flex-1  border-0 select-none outline-none focus:outline-0 ">
     <option value="fisico">Fisico</option>
     <option value="juridico" selected>Juridico</option>
     <option value="" selected></option>
@@ -62,7 +63,7 @@ const NewTiket = () => {
 
 <div className="flex flex-col p-4">
 <label className="px-3">Observação:</label>
-<textarea  {...register("observacao", { required: false })} className="bg-slate-100 flex w-full border-2 border-solid border-black select-none outline-none focus:outline-0 h-full " />
+<textarea  {...register("observacao", { required: false })} className="form-textarea text-black rounded-full select-none outline-none focus:outline-0 h-full " />
 {errors.name && <span>Nome</span>}
 </div>
 
