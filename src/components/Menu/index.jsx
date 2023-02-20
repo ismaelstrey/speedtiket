a55491d6ac55ad1a01e78a2c46bf246014e71bb2
Ismael Strey Pereira <ismaelstrey@hotmail.com>
2023-02-20 11:38:10 -0300

    update form modal

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -2,23 +2,29 @@ import { useState } from "react";
 import {CgMenuRound} from 'react-icons/cg'
 import {IoIosAddCircle} from 'react-icons/io'
 import {BiShowAlt} from 'react-icons/bi'
-import {CiCircleRemove} from 'react-icons/ci'
+import {AiFillDelete} from 'react-icons/ai'
 import {FaThList} from 'react-icons/fa'
+import NewTiket from "../Tikets/NewTiket/NewTiket";
+
 
 const Menu = ()  =>  {
 
   const [menu, setMenu] =useState(true)
+  const [abrirForm, setAbrirForm] =useState(false)
+
+  const AbrirFormulario = () => <div className="fixed flex w-full h-screen items-center justify-center bg-opacity-30 backdrop-blur-sm inset-0"><NewTiket/></div>
 
 return(
   
  
 <div>
+  {abrirForm && <AbrirFormulario />}
    <div className="fixed bottom-10 right-10 rounded-full bg-green-400 p-2 hover:bg-green-900 cursor-pointer z-20" onClick={()=>setMenu(!menu)}><CgMenuRound/>
 </div>
      {menu &&  <div className=" bg-black fixed p-10 rounded-full  bottom-4 right-4 z-10 border-2 border-white ">
-   <span className="bg-blue-400 p-2 rounded-full fixed bottom-10 right-20 hover:border-2 hover: border-white cursor-pointer"><IoIosAddCircle/></span>
+   <span className="bg-green-600 p-2 rounded-full fixed bottom-10 right-20 hover:border-2 hover: border-white cursor-pointer" onClick={()=>setAbrirForm(!abrirForm)}><IoIosAddCircle/></span>
     <span className="bg-red-400 p-2 rounded-full fixed bottom-20 right-10 hover:border-2 hover: border-white cursor-pointer"><BiShowAlt/></span>   
-   <span className="bg-yellow-400 p-2 rounded-full fixed bottom-0 right-10 hover:border-2 hover: border-white cursor-pointer"><CiCircleRemove/></span>
+   <span className="bg-yellow-400 p-2 rounded-full fixed bottom-0 right-10 hover:border-2 hover: border-white cursor-pointer"><AiFillDelete/></span>
     <span className="bg-orange-400 p-2 rounded-full fixed bottom-10 right-0 hover:border-2 hover: border-white cursor-pointer"><FaThList /></span>
     </div>}
   
--- src/components/Tikets/NewTiket/NewTiket.jsx
+++ src/components/Tikets/NewTiket/NewTiket.jsx
@@ -1,30 +1,25 @@
 import { useForm } from "react-hook-form";
 
-
 const NewTiket = () => {
 
   //declarar os métodos que serão utilizados para manipular o form
 
   const { register, handleSubmit, watch, formState: { errors }, reset } = useForm();
-
   //declarar para qual função o método handleSubmit irá enviar as informações
-
   const loginUser = data => console.log(data);
-
   return (
-
     //no onSubmit do form o método handleSubmit irá manipular as informações para o loginUser
 
     //o register recebe o nome do input, e os errors apresentam na tela os erros de validação
 
-    <form onSubmit={handleSubmit(loginUser)} className=" flex justify-center content-center items-center">
+    <form onSubmit={handleSubmit(loginUser)} className="m-20  flex w-full justify-center content-center items-center bg-slate-100 rounded  border-2 border-black">
 
-    <div className="flex flex-col w-3/4  ">
+    <div className="flex flex-col w-full ">
     <div className="flex w-full">
 
 <div className="flex w-full  p-4 content-center items-start justify-start flex-1">
   <label className="px-3 ">Tipo:</label>
-  <select className="bg-slate-100 flex  border-b-2 border-solid border-black select-none outline-none focus:outline-0 ">
+  <select {...register("tipo_empresa", { required: true })}  className="bg-slate-100 flex  border-b-2 border-solid border-black select-none outline-none focus:outline-0 ">
     <option value="fisico">Fisico</option>
     <option value="juridico" selected>Juridico</option>
     <option value="" selected></option>
@@ -47,17 +42,17 @@ const NewTiket = () => {
 <div className="flex w-full">
 <div className="flex p-4 w-full ">
   <label className="px-3">CNPJ:</label>
-  <input  {...register("name", { required: true })} className="bg-slate-100 flex flex-1  border-b-2 border-solid border-black select-none outline-none focus:outline-0 " />
+  <input  {...register("cnpj", { required: true })} className="bg-slate-100 flex flex-1  border-b-2 border-solid border-black select-none outline-none focus:outline-0 " />
   {errors.name && <span>CNPJ</span>}
 </div>
 <div className="flex p-4 w-full">
   <label className="px-3">IE:</label>
-  <input  {...register("name", { required: true })} className="bg-slate-100 flex  flex-1 border-b-2 border-solid border-black select-none outline-none focus:outline-0 " />
+  <input  {...register("ie", { required: true })} className="bg-slate-100 flex  flex-1 border-b-2 border-solid border-black select-none outline-none focus:outline-0 " />
   {errors.name && <span>IE</span>}
 </div>
 <div className="flex w-full  p-4 content-center items-start justify-start flex-1">
   <label className="px-3 ">Matriz:</label>
-  <select className="bg-slate-100 flex flex-1  border-b-2 border-solid border-black select-none outline-none focus:outline-0 ">
+  <select {...register("matriz", { required: true })}  className="bg-slate-100 flex flex-1  border-b-2 border-solid border-black select-none outline-none focus:outline-0 ">
     <option value="fisico">Fisico</option>
     <option value="juridico" selected>Juridico</option>
     <option value="" selected></option>
@@ -67,7 +62,7 @@ const NewTiket = () => {
 
 <div className="flex flex-col p-4">
 <label className="px-3">Observação:</label>
-<textarea  {...register("name", { required: true })} className="bg-slate-100 flex w-full border-2 border-solid border-black select-none outline-none focus:outline-0 h-full " />
+<textarea  {...register("observacao", { required: false })} className="bg-slate-100 flex w-full border-2 border-solid border-black select-none outline-none focus:outline-0 h-full " />
 {errors.name && <span>Nome</span>}
 </div>
 
