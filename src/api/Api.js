ab671148af0da71826b7ebde4ceba1648aef8190
Ismael Strey Pereira <ismaelstrey@hotmail.com>
2023-02-21 11:31:18 -0300

    update api Online

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+const url = `https://api.jsonbin.io/v3/b/63f4cf86c0e7653a057bc4a5`
+const options = {
+    headers:{
+        "X-Master-Key":"$2b$10$/26q/3HIgBcqKaOku5XDZuiF4O7z25h3bJNIcZCKC3F6Z0TJRj3/W"
+    }
+}
 
-const getTiketApi = async () => await axios.get("http://localhost:3000/tiket");
+const getTiketApi = async () => await axios.get(url,options);
+// const getTiketApi = async () => await axios.get("http://localhost:3000/tiket");
 const { data } = await getTiketApi();
-const dataApiTiket = data;
 
-export { dataApiTiket };
+
+const {cliente,tiket} = data.record
+
+export { cliente, tiket };
