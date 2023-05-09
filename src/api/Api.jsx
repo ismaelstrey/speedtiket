b7803e5e9f08978f8628f70e8e5449763225ffe0
Ismael Strey Pereira <ismaelstrey@hotmail.com>
2023-05-08 23:08:04 -0300

    update

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { v4 } from "uuid";
+import moment from "moment/moment";
 // const url = `https://api.jsonbin.io/v3/b/63f4cf86c0e7653a057bc4a5`;
 const options = {
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
   }
 }
 
@@ -10,12 +13,22 @@ const options = {
 const getTiketApi = async () => await axios.get('https://tiket-back.vercel.app/tiket');
 // const getClienteApi = async () => await axios.get('http://localhost:3000/cliente');
 const postTiketApi = async (array) => {
+  const dalataList = {
+    "id": v4(),
+    ...array,
+    "status": "retornados",
+    "tecnico": "Antonio",
+    "created_at": moment().format('L'),
+    "updated_at": moment().format('L'),
+
+  }
   console.log(array)
-  const { data } = await axios.post('https://numerous-sugared-enthusiasm.glitch.me/tiket', {
-    array
+  const { data } = await axios.post('http://localhost:3000/tiket', {
+    ...dalataList
   }, {
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
     }
   }
   )
--- src/components/Tikets/NewTiket/NewTiket.jsx
+++ src/components/Tikets/NewTiket/NewTiket.jsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { postTiketApi } from "../../../api/Api";
 import { v4 } from "uuid";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import moment from "moment/moment";
 import { FormContext } from "../../../usecontext/FormContext";
 
@@ -13,127 +13,159 @@ const NewTiket = () => {
   const { mostrarFormularioTiket } = useContext(FormContext)
   const [id, setId] = useState(v4())
   const [data, setData] = useState(moment().format('L'))
+  const [prioridade, setPrioridade] = useState('Medio')
+  const [color, setColor] = useState()
 
+useEffect(()=>{
+  console.log(prioridade)
+  switch (prioridade) {    
+    case 'alto':
+      setColor('bg-red-500');
+      break
+    case 'critico':
+      setColor('bg-red-800');
+      break
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-    reset,
-  } = useForm();
-  //declarar para qual função o método handleSubmit irá enviar as informações
-  const addTcket = async (data) => await postTiketApi(data)
-  return (
+    case 'Medio':
+      setColor('bg-orange-500');
+      break
 
-    <form
-      onSubmit={handleSubmit(addTcket)}
-      className={`m-20 container text-lg text-white  flex w-full justify-center content-center items-center bg-slate-800 rounded-2xl  border-2 border-black `}
-    >
-      <div className="flex flex-col w-full">
-        <span className="flex w-full items-center justify-center content-center mb-10 bg-blue-700 rounded-t-2xl h-10 border-b-2 border-white">
-          <h2 className="uppercase">novo ticket {moment().format('LTS')}</h2>
-        </span>
-        <div className="flex sm:flex-col lg:flex-row w-full">
-          <div className="flex w-full  p-4 content-center items-center justify-center min-[320px]:items-start min-[320px]:flex-col">
-            <label className="px-3 ">Problema Informado:</label>
-            <input
-              {...register("problema_informado", { required: true })}
-              className="min-[320px]:w-full form-input text-black rounded-full  flex-1   border-0 border-solid  select-none outline-none focus:outline-0"
+    case 'baixo':
+      setColor('bg-green-500');
+      break
+
+    case 'planejado':
+      setColor('bg-blue-500');
+      break
+
+    default:
+      setColor('bg-white');
+  }
+},[prioridade])
+
+console.log(color)
+
+    const {
+      register,
+      handleSubmit,
+      watch,
+      formState: { errors },
+      reset,
+    } = useForm();
+    //declarar para qual função o método handleSubmit irá enviar as informações
+    const addTcket = async (data) => await postTiketApi(data)
+    console.log(data)
+    return (
+
+      <form
+        onSubmit={handleSubmit(addTcket)}
+        className={`m-20 container text-lg text-white  flex w-full justify-center content-center items-center bg-slate-800 rounded-2xl  border-2 border-black `}
+      >
+        <div className="flex flex-col w-full">
+          <span className="flex w-full items-center justify-center content-center mb-10 bg-blue-700 rounded-t-2xl h-10 border-b-2 border-white">
+            <h2 className="uppercase">novo ticket {moment().format('LTS')}</h2>
+          </span>
+          <div className="flex sm:flex-col lg:flex-row w-full">
+            <div className="flex w-full  p-4 content-center items-center justify-center min-[320px]:items-start min-[320px]:flex-col">
+              <label className="px-3 ">Problema Informado:</label>
+              <input
+                {...register("problema_informado", { required: true })}
+                className="min-[320px]:w-full form-input text-black rounded-full  flex-1   border-0 border-solid  select-none outline-none focus:outline-0"
+              />
+
+              {errors.name && <span>Nome</span>}
+            </div>
+          </div>
+          {/* inicio */}
+          <div className="flex w-full min-[320px]:flex-col sm:flex-col lg:flex-row">
+            <div className="flex min-[320px]:flex-col w-full  p-4 content-center items-center justify-start min-[320px]:items-start">
+              <label className="px-3">Cliente:</label>
+              <select
+                {...register("cliente", { required: true })}
+                className="flex w-full text-black form-select flex-1  border-solid  select-none outline-none focus:outline-0 rounded-full border-0 ">
+                <option value="SpeedSul">Speedsul</option>
+                <option defaultValue="Oi Telecom">Oi Telecom</option>
+              </select>
+            </div>
+            <div className="flex w-full  p-4 content-center items-center justify-start">
+              <label className="px-3 ">Categorias:</label>
+              <select
+                {...register("categoria", { required: true })}
+                className="form-select text-black rounded-full flex-1  border-0 select-none outline-none focus:outline-0 "
+              >
+                <option defaultValue="redes">Redes</option>
+                <option value="projeto">Projeto</option>
+                <option value="implantacao">Implatação</option>
+                <option value="problema">Problema</option>
+              </select>
+            </div>
+            <div className="flex w-full  p-4 content-center items-center justify-start">
+              <label className="px-3 ">Subcategoria:</label>
+              <select
+                {...register("subcategoria", { required: true })}
+                className="form-select text-black text-lg rounded-full flex-1  border-0 select-none outline-none focus:outline-0 "
+              >
+                <option value="critico">Configuração</option>
+                <option value="alto">Dividas</option>
+                <option defaultValue="medio">Incidadente</option>
+                <option value="baixo">Mudança</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="flex w-full sm:flex-col lg:flex-row">
+            <div className="flex w-full  p-4 content-center items-center justify-start">
+              <label className="px-3 ">Prioridade:</label>
+              <select
+                {...register("prioridade", { required: true })}
+                onChange={(e) => setPrioridade(e.target.value)}
+                className={`form-select text-black text-lg rounded-full flex-1  border-0 select-none outline-none focus:outline-0 ${color}`}
+              >
+                <option value="critico" className="bg-red-700 text-white" >Critico</option>
+                <option value="alto" className="bg-red-700 text-white">
+                  Alto
+                </option>
+                <option defaultValue="medio" className="bg-red-400 text-white">
+                  Medio
+                </option>
+                <option value="baixo" className="bg-violet-700 text-white">
+                  Baixo
+                </option>
+                <option value="planejado" className="bg-green-500 text-white">
+                  Planejado
+                </option>
+              </select>
+            </div>
+          </div>
+          {/* end  */}
+          <div className="flex flex-col p-4">
+            <label className="px-3">Observação:</label>
+            <textarea
+              {...register("observacao", { required: false })}
+              maxLength="400"
+              rows={5}
+              className="form-textarea text-black rounded select-none outline-none focus:outline-0 h-full "
             />
-
             {errors.name && <span>Nome</span>}
           </div>
-        </div>
-        {/* inicio */}
-        {/* <div className="flex w-full min-[320px]:flex-col sm:flex-col lg:flex-row">
-          <div className="flex min-[320px]:flex-col w-full  p-4 content-center items-center justify-start min-[320px]:items-start">
-            <label className="px-3">Cliente:</label>
-            <select
-              {...register("cliente", { required: true })}
-              className="flex w-full text-black form-select flex-1  border-solid  select-none outline-none focus:outline-0 rounded-full border-0 ">
-              <option value="fisico">Speedsul</option>
-              <option defaultValue="juridico">Oi Telecom</option>
-            </select>
-          </div>
-          <div className="flex w-full  p-4 content-center items-center justify-start">
-            <label className="px-3 ">Categorias:</label>
-            <select
-              {...register("categoria", { required: true })}
-              className="form-select text-black rounded-full flex-1  border-0 select-none outline-none focus:outline-0 "
-            >
-              <option defaultValue="redes">Redes</option>
-              <option value="projeto">Projeto</option>
-              <option value="implantacao">Implatação</option>
-              <option value="problema">Problema</option>
-            </select>
-          </div>
-          <div className="flex w-full  p-4 content-center items-center justify-start">
-            <label className="px-3 ">Subcategoria:</label>
-            <select
-              {...register("subcategoria", { required: true })}
-              className="form-select text-black text-lg rounded-full flex-1  border-0 select-none outline-none focus:outline-0 "
-            >
-              <option value="critico">Configuração</option>
-              <option value="alto">Dividas</option>
-              <option defaultValue="medio">Incidadente</option>
-              <option value="baixo">Mudança</option>
-            </select>
-          </div>
-        </div>
 
-        <div className="flex w-full sm:flex-col lg:flex-row">
-          <div className="flex w-full  p-4 content-center items-center justify-start">
-            <label className="px-3 ">Prioridade:</label>
-            <select
-              {...register("prioridade", { required: true })}
-              className="form-select text-black text-lg rounded-full flex-1  border-0 select-none outline-none focus:outline-0 "
-            >
-              <option value="critico">Critico</option>
-              <option value="alto" className="bg-red-700 text-white">
-                Alto
-              </option>
-              <option defaultValue="medio" className="bg-red-400 text-white">
-                Medio
-              </option>
-              <option value="baixo" className="bg-violet-700 text-white">
-                Baixo
-              </option>
-              <option value="planejado" className="bg-green-500 text-white">
-                Planejado
-              </option>
-            </select>
+          <div className="flex w-full justify-end">
+            <span className="m-4">
+              <input
+                type="submit"
+                className="border-2 border-black bg-green-500 text-white p-2 mr-2 rounded cursor-pointer"
+              />
+              <input
+                type="button"
+                onClick={() => mostrarFormularioTiket()}
+                value="Cancelar"
+                className="border-2 border-black pt-2 bg-red-500 text-white p-2 rounded cursor-pointer"
+              />
+            </span>
           </div>
-        </div> */}
-        {/* end  */}
-        <div className="flex flex-col p-4">
-          <label className="px-3">Observação:</label>
-          <textarea
-            {...register("observacao", { required: false })}
-            maxLength="400"
-            rows={5}
-            className="form-textarea text-black rounded select-none outline-none focus:outline-0 h-full "
-          />
-          {errors.name && <span>Nome</span>}
         </div>
+      </form>
+    );
+  };
 
-        <div className="flex w-full justify-end">
-          <span className="m-4">
-            <input
-              type="submit"
-              className="border-2 border-black bg-green-500 text-white p-2 mr-2 rounded cursor-pointer"
-            />
-            <input
-              type="button"
-              onClick={() => mostrarFormularioTiket()}
-              value="Cancelar"
-              className="border-2 border-black pt-2 bg-red-500 text-white p-2 rounded cursor-pointer"
-            />
-          </span>
-        </div>
-      </div>
-    </form>
-  );
-};
-
-export default NewTiket;
+  export default NewTiket;
